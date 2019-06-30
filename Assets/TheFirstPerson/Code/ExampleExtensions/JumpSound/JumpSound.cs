c6fd4735f5870bb726016a0599803e9a4c61d375
boaheck <breogan.hackett2@mail.dcu.ie>
2019-06-30 19:17:20 +0100

    add jump sfx extension

---
+++ Assets/TheFirstPerson/Code/ExampleExtensions/JumpSound/JumpSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheFirstPerson;
+public class JumpSound : TFPExtension{
+
+    public AudioSource src;
+    public AudioClip sound;
+
+    public override void ExPostUpdate(ref TFPData data, TFPInfo info){
+        if(data.timeSinceGrounded <= Time.deltaTime && data.jumping){
+            src.PlayOneShot(sound);
+        }
+    }
+    
+}
